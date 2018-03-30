a65fb592a5f25ca963ab6bab798299d51bd15aa7
Snez0 <Snez0@DESKTOP-RON656Q>
2018-03-30 10:28:27 +0300

    Fixed #14

--- HW1Strings/Program.cs
+++ HW1Strings/Program.cs
@@ -26,9 +26,9 @@ namespace HW1Strings
             string[] words = example.Split(new char[] { ' ' });
             for (int i = 0; i < words.Length; i++)
             {
-                foreach (string ToCheck in list)
+                foreach (string toCheck in list)
                 {
-                    if (words[i].Equals(ToCheck))
+                    if (words[i].Equals(toCheck))
                         words[i] = words[i].ToLower();
                 }
             }
